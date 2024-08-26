8d87869b535927866760927b3e7cc04e5b3aaace
SerjMolodoy <vipper_87@mail.ru>
2024-08-26 17:23:16 +0300

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,20 @@
+count_of_completed_homework = 12
+hours_spent = 1.5
+name_of_course = 'Python'
+time_per_task = hours_spent / count_of_completed_homework
+print('Курс:', name_of_course, ', всего задач: ', count_of_completed_homework, ', затрачено часов: ',
+      hours_spent, ', среднее время выполнения: ', time_per_task, 'часа.')
+print()
+nothing_doing_months = 1
+days = 31
+hours = 24
+completed_homeworks = 4
+name_of_course = 'Python'
+name = 'Serj'
+status = 'бездельник'
+time = nothing_doing_months * days * hours
+time_per_task = time / completed_homeworks
+print('Курс:', name_of_course, ', всего задач: ', completed_homeworks, ', затрачено часов: ', time,
+            ', среднее время выполнения: ', time_per_task, 'часов.' ' Итог: ', name, status, '!')
+print()
+print('Всё!')
