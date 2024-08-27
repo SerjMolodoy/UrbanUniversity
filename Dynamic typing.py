0bfd2e1721c9d942dbbd4978e950cf7528a78fd1
SerjMolodoy <vipper_87@mail.ru>
2024-08-27 09:33:50 +0300

    Add files via upload

---
+++ Dynamic typing.py
@@ -0,0 +1,20 @@
+name = 'Sergei'
+print('Name:', name)
+age = 37
+print('Age:', age)
+age = age + 1
+print('New age', age)
+is_student = True
+print('Is student:', is_student)
+print()
+year_of_birth = 1987
+print('Year of birth:', year_of_birth)
+current_year = 2024
+print('Current year:', current_year)
+age = current_year - year_of_birth
+print('Age:', age)
+next_year = 2025
+age = next_year - year_of_birth
+print('New age:', age)
+is_old = False
+print('Is old:', is_old)
