89376c4ae587434c10701e3a7bdff269164b6af6
SerjMolodoy <vipper_87@mail.ru>
2024-08-29 16:18:27 +0300

    Add files via upload

---
+++ If else elif.py
@@ -0,0 +1,32 @@
+first = int(input('Первое число: '))
+second = int(input('Второе число: '))
+third = int(input('Третье число: '))
+if first == second == third:
+    print(3)
+if second == third or first == third or second == first:
+    print(2)
+else:
+    print(0)
+
+# print()
+# x = int(input('Введите число: '))
+# if x <= 0:
+#     print('Few')
+# if x % 5 == 0:
+#     print('Good')
+# elif x % 2 == 0 or x % 4 == 0:
+#     print('Not bad')
+# if x % 2 == 0 or x % 4 == 0:
+#     print('Very good')
+# elif x % 2 == 0 and x % 4 == 0:
+#     print('Ok')
+# if x % 2 == 0 and x % 4 == 0:
+#     print('Excellent')
+# else:
+#     print('Bad')
+# print()
+# print('Всё!')
+
+
+
+
