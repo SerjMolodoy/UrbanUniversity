53bbd94462fe01d3c8f4e316f49018fb9928c0fe
SerjMolodoy <vipper_87@mail.ru>
2025-02-11 00:01:30 +0300

    Add files via upload

---
+++ module_13_2.py
@@ -0,0 +1,33 @@
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.enums import ParseMode
+from aiogram.filters import Command
+from aiogram.client.default import DefaultBotProperties
+
+# Устанавливаем уровень логирования
+logging.basicConfig(level=logging.INFO)
+
+# Инициализация бота и диспетчера
+API_TOKEN = 'АПИ'  #Ваш токен
+
+# Используем DefaultBotProperties для настройки parse_mode
+bot = Bot(token=API_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+dp = Dispatcher()  # Dispatcher больше не принимает бота в конструкторе
+
+# Обработчик команды /start
+@dp.message(Command("start"))
+async def start(message: types.Message):
+    print('Привет! Я бот, помогающий твоему здоровью.')
+
+# Обработчик всех остальных сообщений
+@dp.message()
+async def all_messages(message: types.Message):
+    print('Введите команду /start, чтобы начать общение.')
+
+# Запуск бота
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    import asyncio
+    asyncio.run(main())
